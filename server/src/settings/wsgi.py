12386fa7a158951e6149c2124fb9dccb118bf5e2
sumitpathak92 <pathaksumit92@gmail.com>
2016-04-19 15:23:58 +0530

    Removed Incorrect project name wherever found.

--- server/src/settings/wsgi.py
+++ server/src/settings/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AIlifesaver project.
+WSGI config for AllRounder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
--- server/src/urls.py
+++ server/src/urls.py
@@ -1,4 +1,4 @@
-"""AIlifesaver URL Configuration
+"""AllRounder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
